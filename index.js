633808be4c16c24f558394de6d859a7cad47c070
Edmond Pruteanu <edmondpr@gmail.com>
2016-04-17 00:45:01 +0100

    Replaced index.js file

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-var cool = require('cool-ascii-faces');
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
 
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('It works!');
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.get(['/facebook', '/instagram'], function(req, res) {
+  if (
+    req.param('hub.mode') == 'subscribe' &&
+    req.param('hub.verify_token') == 'token'
+  ) {
+    res.send(req.param('hub.challenge'));
+  } else {
+    res.sendStatus(400);
+  }
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.post('/facebook', function(req, res) {
+  console.log('Facebook request body:');
+  console.log(req.body);
+  // Process the Facebook updates here
+  res.sendStatus(200);
 });
+
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  // Process the Instagram updates here
+  res.sendStatus(200);
+});
+
+app.listen();
